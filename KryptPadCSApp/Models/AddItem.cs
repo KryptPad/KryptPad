74e960c32e6571f82de45a5b748a0da7cc877753
Eric Butler <neptunecentury@hotmail.com>
2015-08-20 20:41:21 -0400

    Added AddItem

---
+++ KryptPadCSApp/Models/AddItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace KryptPadCSApp.Models
+{
+    class AddItem : BaseModel, IItem
+    {
+        private string _name;
+        /// <summary>
+        /// Gets or sets the name of the profile
+        /// </summary>
+        public string Name
+        {
+            get
+            { return _name; }
+            set
+            {
+                _name = value;
+                //raise change event
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private SymbolIcon _symbol;
+        /// <summary>
+        /// Gets or sets the category symbol from font icon set
+        /// </summary>
+        public SymbolIcon Symbol
+        {
+            get { return _symbol; }
+            set
+            {
+                _symbol = value;
+                //raise change event
+                OnPropertyChanged(nameof(Symbol));
+            }
+        }
+    }
+}
--- KryptPadCSApp/Models/IItem.cs
+++ KryptPadCSApp/Models/IItem.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace KryptPadCSApp.Models
 {
     interface IItem
     {
         string Name { get; set; }
+        SymbolIcon Symbol { get; set; }
     }
 }
