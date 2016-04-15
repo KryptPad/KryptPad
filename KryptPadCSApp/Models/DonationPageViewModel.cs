8821add74370b174a3f9e43385bffa00ec1b71a5
Eric Butler <neptunecentury@hotmail.com>
2016-04-14 21:13:29 -0400

    Update to donation url

--- KryptPadCSApp/Models/DonationPageViewModel.cs
+++ KryptPadCSApp/Models/DonationPageViewModel.cs
@@ -29,7 +29,7 @@ namespace KryptPadCSApp.Models
             {
                 // Launch the uri
                 await Windows.System.Launcher.LaunchUriAsync(
-                    new Uri("https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&SESSION=EHNMjs-f8NSU_h1EkfBswaKbAWOC9Mu-bo1Hvydnh-MrSsDocpenyvZ3pq4&dispatch=5885d80a13c0db1f8e263663d3faee8d6625bf1e8bd269586d425cc639e26c6a"));
+                    new Uri("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5547784"));
 
             });
         }
